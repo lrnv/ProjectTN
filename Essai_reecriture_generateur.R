25ac4e88e60e3c6eb54cc95338a58978f2545f76
lrnv <oskar.laverny@gmail.com>
2017-12-08 13:25:24 +0100

    Re-mise en forme des générateurs

--- Essai_reecriture_generateur.R
+++ Essai_reecriture_generateur.R
@@ -1,89 +1,148 @@
 # Essai de réécriture
 
 
-rPDD <- function(n=1,S0=100,Sa=120,delta_t = 1/365,alpha=0.2,r=0.015,sigma=0.45,raffinement=FALSE, Temps = 1){
-  
-  # Probabilité de franchissement du seuil dans le cas de pont brownien : 
-  p_k <- function(x,y){ return(exp(-2*log(x/Sa)*log(y/Sa)/(sigma^2*delta_t))) }
-  
-  # Calcul d'une PDD : 
-  rPDD_unitaire <- function(naif,S0,Sa){
-    
-    # Chacun des lambdas dépends de la valeur de l'actif en début de periode et 
-    # de la valeur du lambda précédent. Le rete n'est que simulation de l'actif. 
-    
-    lambda <- function(S0,lambdaPrecedent = 0){
+##################################################################
+###################### Fonction .rPDD_unitaire : générateur de PDD à aléa paramètrable.
+##################################################################
+.rPDD_unitaire <- function(naif=TRUE, S0=100, Sa=120, delta_t = 1/365,
+                           alpha=0.2, r=0.015, sigma=0.45, Temps = 1, alea){
       
-      # Commençons par calculer la trajectoire de S : 
-      # S suit une dynamique de black-sholes, donc la solution explicite de BS nous donne :
-      alea <- rnorm(1/delta_t)
-      S <- S0 * cumprod(1+r * delta_t + sigma * sqrt(delta_t) * alea)
-      S <- S*(S > 0) # Histoire que le sous-jacent reste bien positif.
-      
-      # Gestion du raffinement par pont brownien : 
-      p = S^0
-      if(!naif){
-        p <- c(1,(p_k(c(NA,S),c(S,NA))[2:(length(S)-1)]))
-        p <- p > runif((1/delta_t)-1) # donc p = true quand la barrière désactivante n'est pas franchie. 
-        p[1:(floor(1/delta_t)+1)] <- TRUE # Les 6 premiers mois sont toujours valides.
-      }
-      
-      # Maintenant qu'on a la trajectoire de S, calculons la PDD_0 correspondante : 
-      # Approximation du sup sur 6mois : 
-      sup_6_mois <- max(S[(floor(length(S)/2)+1):length(S)])
-      # Calcul final de la PDD : 
-      lambda <-prod(p)*max(Sa-lambdaPrecedent-S[length(S)],0) * (S[length(S)] < (1-alpha) * Sa) * (sup_6_mois < Sa) 
-      return(c(S[length(S)],lambda))
-    }
-    
-    lambda_rez <- list()
-    lambda_rez[[1]] <- lambda(S0,0)
-    if(Temps > 1){
-      for (i in 2:Temps){
-        lambda_rez[[i]] <- lambda(lambda_rez[[i-1]][1],lambda_rez[[i-1]][2])
-        # La formule est juste la même : 
-        # On relance avec un nouveau point de départ S0 correspondant a la derière valeur obtenue au cout d'avent, 
-        # et le lambda précédent qui est 0 au début.
-      }
-    }
-    lambda_rez %>% 
-      lapply(function(x) return(x[[2]])) %>%
-      unlist %>%
-      # tail(1) %>% 
-      multiply_by(exp(-r * (1:Temps))) %>%
-      tail(1) %>%
-      return
-  }
+      # Le paramètres "aléa" attend une rélisation d'une variable aléatoire N(0,1) de dimention Temps/delta_t
   
-  # On retourne le résultat :
-  return(rep(NA,n) %>% 
-           sapply(.,function(x){
-             return(rPDD_unitaire(naif=!raffinement,S0=S0,Sa=Sa))
-           }
-           ))
+      # Probabilité de franchissement du seuil dans le cas de pont brownien : 
+        p_k <- function(x,y){ 
+          return(exp(-2*log(x/Sa)*log(y/Sa)/(sigma^2*delta_t))) 
+        }
+        
+        
+      # Chacun des lambdas dépends de la valeur de l'actif en début de periode et 
+      # de la valeur du lambda précédent. Le rete n'est que simulation de l'actif. 
+        lambda <- function(S0,lambdaPrecedent = 0,periode=1){
+          
+          # Commençons par calculer la trajectoire de S : 
+          # S suit une dynamique de black-sholes, donc la solution explicite de BS nous donne :
+          PetitAlea <- alea[(1 + (periode-1)/delta_t):(periode/delta_t)]
+          S <- S0 * cumprod(1+r * delta_t + sigma * sqrt(delta_t) * PetitAlea)
+          S <- S*(S > 0) # Histoire que le sous-jacent reste bien positif.
+          
+          # Gestion du raffinement par pont brownien : 
+          p = S^0
+          if(!naif){
+            p <- c(1,(p_k(c(NA,S),c(S,NA))[2:(length(S)-1)]))
+            p <- p > runif((1/delta_t)-1) # donc p = true quand la barrière désactivante n'est pas franchie. 
+            p[1:(floor(1/delta_t)+1)] <- TRUE # Les 6 premiers mois sont toujours valides.
+          }
+          
+          # Maintenant qu'on a la trajectoire de S, calculons la PDD_0 correspondante : 
+          # Approximation du sup sur 6mois : 
+          sup_6_mois <- max(S[(floor(length(S)/2)+1):length(S)])
+          # Calcul final de la PDD : 
+          lambda <-prod(p)*max(Sa-lambdaPrecedent-S[length(S)],0) * (S[length(S)] < (1-alpha) * Sa) * (sup_6_mois < Sa) 
+          return(c(S[length(S)],lambda))
+        }
+      
+      # Application de la fonction lambda récursivement : 
+        lambda_rez <- list()
+        lambda_rez[[1]] <- lambda(S0 = S0,lambdaPrecedent = 0,periode=1)
+        if(Temps > 1){
+          for (i in 2:Temps){
+            lambda_rez[[i]] <- lambda(S0 = lambda_rez[[i-1]][1],lambdaPrecedent = lambda_rez[[i-1]][2], periode = i)
+            # La formule est juste la même : 
+            # On relance avec un nouveau point de départ S0 correspondant a la derière valeur obtenue au cout d'avent, 
+            # et le lambda précédent qui est 0 au début.
+          }
+        }
+      
+      # Récupération des résultat, somme et actualisation pour récupérer la PDD : 
+        lambda_rez %>% 
+          lapply(function(x) return(x[[2]])) %>%
+          unlist %>%
+          multiply_by(exp(-r * (1:Temps))) %>% # Actualisation des différents lambdas.
+          tail(1) %>%
+          return
 }
 
-# Petite fonction d'affichage : 
-affichage <- . %>% 
-{data.frame(Moyenne = mean(.),ProbaPositif = mean(. > 0))} #%>%
-#{print(.)}
+##################################################################
+###################### Fonction rPDD : générateur de PDD.
+##################################################################
+rPDD <- function(n=1, S0=100, Sa=120, delta_t = 1/365, alpha=0.2, 
+                 r=0.015, sigma=0.45, raffinement=FALSE, Temps = 1){
+  
+  # On applique juste plusieurs fois la fonction .rPDD_unitaire, avec à chaque fois un nouvel alléa.
+  # FOnction a apply plusieurs fois : 
+  to_apply <- function(x){
+    return(
+      .rPDD_unitaire(
+        naif=!raffinement,
+        S0=S0,
+        Sa=Sa,
+        delta_t = delta_t,
+        alpha=alpha,
+        r=r,
+        sigma=sigma,
+        Temps = Temps,
+        alea=rnorm(Temps/delta_t)
+      )
+    )
+  }
+  rep(NA,n) %>% sapply(.,to_apply) %>% return
+}
 
-M = 100
-list(
-  # Test : ( devrais retourner 23/24/25)
-  NaifT1 = rPDD(M) %>% affichage,
+
+
+
+# Générateur aléatoire des greeques. Le paramètre "greek" doit être "Vega","Rho","Delata" ou "Saga"
+rGreek <- function(n=1,greek="Vega",S0=100,Sa=120,delta_t = 1/365,alpha=0.2,r=0.015,
+                   sigma=0.45,raffinement=FALSE, Temps = 1, delta_theta=10^(-5)){
+  
+  # Chaque greeque est une dérivée selon un certain paramètre, avec un alea fixé. 
+  # on a juste besoin de passer en paramètres de .rPDD_unitaire les mêmes paramètres, 
+  # avec un +/- delta_theta pour le paramètre choisis, 
+  # et le même aléa dans les deux fonctions.
+  
+  # On a plus qu'a retourner la différence finie pour la greque voulue, le nombre de fois voulue.
   
   
-  
-  #1.1.2 Méthode par pont brownien : 
-  RafiT1 = rPDD(M,raffinement=TRUE) %>% affichage,
-  
-  #1.1.
-  RafiT2 = rPDD(M,raffinement = TRUE,Temps = 2) %>% affichage,
-  RafiT5 = rPDD(M,raffinement = TRUE,Temps = 5) %>% affichage,
-  RafiT10 = rPDD(M,raffinement = TRUE,Temps = 10) %>% affichage,
-  RafiT15 = rPDD(M,raffinement = TRUE,Temps = 15) %>% affichage,
-  RafiT100 = rPDD(M,raffinement = TRUE,Temps = 100) %>% affichage
-) %>% 
-  do.call(rbind,.) %>% 
-  data.frame
+  to_apply <- function(x){
+    alea <- rnorm(Temps/delta_t)
+    return(
+        (.rPDD_unitaire(
+          S0 = S0              + delta_theta*(greek=="Delta"),
+          Sa = Sa              + delta_theta*(greek=="Saga"),
+          r = r                + delta_theta*(greek=="Rho"),
+          sigma = sigma        + delta_theta*(greek=="Vega"),
+          delta_t = delta_t,
+          alpha = alpha,
+          Temps = Temps,
+          naif = !raffinement,
+          alea = alea
+        )
+        - .rPDD_unitaire(
+          S0 = S0              - delta_theta*(greek=="Delta"),
+          Sa = Sa              - delta_theta*(greek=="Saga"),
+          r = r                - delta_theta*(greek=="Rho"),
+          sigma = sigma        - delta_theta*(greek=="Vega"),
+          delta_t = delta_t,
+          alpha = alpha,
+          Temps = Temps,
+          naif = !raffinement,
+          alea = alea
+        )
+      )/(2*delta_theta)
+    )
+  }
+  rep(NA,n) %>% sapply(.,to_apply) %>% return
+
+}
+
+
+
+
+rGreek(1000,"Vega") %>% plot
+rGreek(1000,"Vega") %>% mean
+rGreek(1000,"Rho") %>% mean
+rGreek(1000,"Delta") %>% mean
+
+rGreek(10000,"Vega") %>% density %>% plot
+
+
